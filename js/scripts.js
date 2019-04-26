e381e8741c2c17402f7ef039bd875510bb5bba62
Samuel <sammythige26@gmail.com>
2019-04-26 13:34:37 +0300

    second condition

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,14 @@ var Answer = function () {
 
   var output = document.getElementById('output');
 
-  
+
   if (one >= two + three || two >= one + three || three >= one + two) {
     alert("The inputed measurements cannot form a triangle");
   }else if (one===two || two===three || one===three){
     alert("This is an isosceles triangle")
+  }else if (one===two && two===three){
+     alert("This is an equilateral triangle")
+  }
+
   }
 }
