0c59b5727d8498a1c3e10bd169665a2bc6c98edd
Samuel <sammythige26@gmail.com>
2019-04-26 12:57:40 +0300

    initial javascript

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var Answer = function () {
+  var  =parseFloat(document.getElementsByName('one').value);
+  var  =parseFloat(document.getElementsByName('two').value);
+  var  =parseFloat(document.getElementsByName('three').value);
+  if (one >= two + three || two >= one + three || three >= one + two) {
+    alert()
+
+  }
+}
