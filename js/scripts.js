eec83f21a5e13c5f72d3135143b2e3963b0f1a9c
Samuel <sammythige26@gmail.com>
2019-04-29 10:55:44 +0300

    changes to code

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,18 @@ var Answer = function () {
   var two =parseInt(document.Number.two.value);
   var three =parseInt(document.Number.three.value);
 
+console.log(one);
+console.log(two);
+console.log(three);
   var output = document.getElementById('output');
 
 
   if (one >= two + three || two >= one + three || three >= one + two) {
     alert("The inputed measurements cannot form a triangle");
   }else if (one===two || two===three || one===three){
-    alert("This is an isosceles triangle");
-  }else if (one===two && two===three){
-     alert("This is an equilateral triangle");
+    alert("This is an equilateral triangle");
+  }else if (one===two && two===three && three!==one || one===two && two!==three && three===one || one!==two && two===three && three===one){
+     alert("This is an isoscoles triangle");
   }else {
     alert("SCALENE TRIANGLE");
   }
